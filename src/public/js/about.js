c36cf078f585630cb18f89e5b698adad0ed57428
dpjames <djames@34north.com>
2018-11-21 22:20:59 -0800

    mobile list view temp

--- src/public/js/about.js
+++ src/public/js/about.js
@@ -4,3 +4,35 @@ function circleHover(who){
    title.classList.toggle("hide");
    content.classList.toggle("hide");
 }
+function fillMobileRows(){
+   var titles = document.querySelectorAll(".circleTitle");
+   var descriptions = document.querySelectorAll(".circleText p");
+   var rows = document.querySelectorAll(".infoRow");
+   var list = document.getElementById("mobileRows");
+   for(var i = 0; i < titles.length; i++){
+      var listEntry = createListEntry(false);
+      listEntry.children[0].innerHTML = titles[i].innerHTML;
+      listEntry.children[1].innerHTML = descriptions[i].innerHTML;
+      list.appendChild(listEntry);
+
+   }
+}
+function createListEntry(image){
+   items = []
+   var li = document.createElement("div");
+   li.classList.add("contentListItem");
+   var title = document.createElement("div");
+   title.classList.add("contentListItemTitle");
+   li.appendChild(title);
+   var description = document.createElement("div");
+   description.classList.add("contentListItemDescription");
+   li.appendChild(description);
+   if(image){
+      var image = document.createElement("div");
+      image.classList.add("contentListItemImage");
+      li.appendChild(image);
+   }
+
+   return li;
+}
+fillMobileRows();
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,10 +4,17 @@ function changePage(where){
       var js = document.createElement("script");
       js.setAttribute("src", "/js/"+where+".js");
       document.getElementsByTagName("head")[0].appendChild(js);
+      hideMobileNav();
    })
    .catch(error => alert(error));
 }
 function getPage(where){
    return fetch('/html/'+where+".html")
          .then(response => response.text());
+}
+function toggleMobileNav(){
+   document.getElementById("nav").classList.toggle("mediahide");
+}
+function hideMobileNav(){
+   document.getElementById("nav").classList.add("mediahide");
 }
