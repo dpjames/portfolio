10d75563d5506f2296d81f804f47b949d69a9e5c
dpjames <djames@34north.com>
2018-11-22 00:03:35 -0800

    adventure list implement

--- src/public/js/adventures.js
+++ src/public/js/adventures.js
@@ -7,7 +7,7 @@ function fillAdventureList(){
       }).catch( err => alert(err));
 }
 function getAdventureData(){
-   return fetch("/adventures").then(resp => resp.json());
+   return fetch("/data/adventures.json").then(resp => resp.json());
 }
 function createAdventureEntry(container){
    return function(adv){
@@ -15,10 +15,29 @@ function createAdventureEntry(container){
       var description = adv.description;
       var image = adv.image;
       var entry = createListEntry(true);
-      entry.children[0].innerHTML = title;
-      entry.children[1].innerHTML = description;
-      entry.children[2].innerHTML = image;
-      container.appendChild(container);
+      entry.children[0].innerHTML = image;
+      entry.children[1].innerHTML = title;
+      entry.children[2].innerHTML = description;
+      container.appendChild(entry);
    };
+}
+function showAdventureForm(){
+   document.getElementById("newAdventureForm").classList.toggle("hide");
+}
+function createNewAdventure(){
+   var info = document.getElementById("newPostData").querySelectorAll("input");
+   var keys = document.getElementById("newPostData").querySelectorAll("label");
+   var dataJson = {}
+   for(var i = 0; i < info.length; i++){
+      dataJson[keys[i].innerHTML] = info[i].value;
+   }
+   console.log(JSON.stringify(dataJson));
+}
+var count = 5;
+function newPostCount(){
+   count--;
+   if(count < 0){
+      document.getElementById("newAdventureForm").classList.remove("hide");
+   }
 }
 fillAdventureList();
