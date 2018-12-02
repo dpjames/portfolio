ab4253506e34f3eb10b13e7eb19a018e93397fcd
dpjames <dpjames@calpoly.edu>
2018-12-01 21:16:45 -0800

    added more edit options and better map functiuons

--- src/public/js/adventures.js
+++ src/public/js/adventures.js
@@ -1,4 +1,3 @@
-console.log("hello world");
 function getAdventureData(){
    return fetch("/data/adventures.json").then(resp => resp.json());
 }
@@ -12,24 +11,42 @@ function changeMapControls(caller){
       caller.classList.add("active");
    }
    const dest = document.getElementById("mapControlsContent"); 
-   dest.innerHTML = document.getElementById(whereTo).innerHTML;
+   dest.innerHTML = document.querySelector(whereTo).innerHTML;
 }
 function populateCallout(feature, callout){
+   const deleteButton = `<button>delete</button>`
    callout.element.innerHTML = 
    `
    <div class="popup">
       <div class="title">${feature.get("title")}</div>
       <div class="description">${feature.get("description")}</div>
       <div class="tags">${feature.get("tags")}</div>
+      <button class="editAdventureGUI" 
+              onclick="deleteAdventure(${feature.get("uid")})">delete
+      </button>
    </div> 
    `;
+}
+function getDelBod(uid){
+   return JSON.stringify({
+      uname : document.getElementById("uname").value,
+      pwd : document.getElementById("pwd").value,
+      adventure: {
+         uid:uid
+      }
+   });
+}
+function deleteAdventure(uid){
+   fetch("/adventure", {method:"DELETE", body:getDelBod(uid)})
+      .then((res) => res.status == 200 ? initMap() : alert(res.status))
+      .catch((err) => alert(err));
 }
 function mapClick(evt){
    let lonlat = ol.proj.toLonLat(evt.coordinate);
    let showCallout = false
-   const mapel = document.getElementById("map");
    document.getElementById("latin").value = lonlat[1]; 
    document.getElementById("lonin").value = lonlat[0]; 
+   const mapel = document.getElementById("map");
    mapel.map.forEachFeatureAtPixel(evt.pixel, function(feature){
       populateCallout(feature, mapel.callout);  
       mapel.callout.setPosition(evt.coordinate);
@@ -39,20 +56,56 @@ function mapClick(evt){
       mapel.callout.setPosition(undefined);
    }
 }
+function generateAdvList(){
+   const map = document.getElementById("map").map;
+   const advlayer = getLayerByName(document.getElementById("map").map, "adventures");
+   const src = advlayer.getSource();
+   const container = document.querySelector(".advList");
+   container.innerHTML = "";
+   src.forEachFeature((feature) => {
+      var thisHTML = 
+      `
+      <div class="contentListItem">
+         <div>${feature.get("title")}</div>
+         <div>${feature.get("description")}</div>
+         <div>${feature.get("tags")}</div>
+      </div>
+      `;
+      container.innerHTML+=thisHTML; 
+   });
+}
 function createAdvLayer(){
    const src = new ol.source.Vector({
       url:"./data/adventures.json",
       format: new ol.format.GeoJSON()
    });
    //maybe cluster
-   return new ol.layer.Vector({
+   const advlayer = new ol.layer.Vector({
       title:"adventures",
       source:src
    });
+   advlayer.on("change", generateAdvList);
+   return advlayer;
+}
+function createTopoLayer(){
+   return new ol.layer.Tile({
+       title: 'OSM',
+       type: 'base',
+       visible: true,
+       source: new ol.source.XYZ({
+           url: '//{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+           //url: '//{a-c}.tile.opentopomap.org/{z}/{x}/{y}.png'
+           //url: 'http://c.tiles.wmflabs.org/hillshading/{z}/{x}/{y}.png'
+           //url: 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg'
+           //url: 'http://tile.thunderforest.com/landscape/{z}/{x}/{y}.png'
+       })
+   });   
 }
 function initMap(){
+   var mapel = document.getElementById("map");
+   mapel.innerHTML = "";
    const layers = [
-      new ol.layer.Tile({source: new ol.source.OSM({crossOrigin : null})}),
+      createTopoLayer(),
       createAdvLayer()
    ];
    const callout = new ol.Overlay({
@@ -69,13 +122,57 @@ function initMap(){
    });
    map.addOverlay(callout);
    map.on("click", mapClick);
-   var mapel = document.getElementById("map");
    mapel.map = map;
+   mapel.removedFeatures = [];
    mapel.callout = callout;
    setTimeout(()=>map.updateSize(), 200);
 }
+function searchFilter(lname, sboxquery){
+   const term = document.querySelector(sboxquery).value;
+   const filterFunction = function(f){
+      const props = f.getProperties()
+      let contains = false;
+      for(let p in props){
+         if(typeof props[p] === "string" && props[p].toLowerCase().indexOf(term.toLowerCase()) > -1){
+            return true;
+         }
+      }
+      return false;
+   }
+   filter(filterFunction, lname);
+}
+function getLayerByName(map, lname){
+   let layer;
+   map.getLayers().forEach((l) => {
+      if(l.get("title") === lname){
+         layer = l;
+      }
+   });
+   return layer;
+}
+function filter(fun, lname){
+   const mapel = document.getElementById("map");
+   const layer = getLayerByName(mapel.map, lname);
+   //essentially array.filter, but that function is not implemented :(
+   const src = layer.getSource();
+   src.forEachFeature(function(feature){
+      if(!fun(feature)){
+         mapel.removedFeatures.push(feature);
+         src.removeFeature(feature);
+      }
+   }); 
+}
+function resetFilters(lname){
+   const mapel = document.getElementById("map");
+   const layer = getLayerByName(mapel.map, lname);
+   const src = layer.getSource();
+   mapel.removedFeatures.forEach(function(feature){
+      src.addFeature(feature); 
+   });
+   mapel.removedFeatures = [];
+}
 function createNewAdventure(){
-   const data = document.getElementById("newAdvTable").querySelectorAll("td input");
+   const data = document.getElementById("newAdvTable").querySelectorAll("td .advInput");
    let body = {adventure :{}};
    data.forEach((e) => {
       const key = e.getAttribute("data-key")
@@ -86,12 +183,12 @@ function createNewAdventure(){
       body["adventure"][key] = e.value; 
    });
    fetch("/adventure", {method:"POST", body:JSON.stringify(body)})
-   .then(resp => resp.status == 200 ? Promise.resolve() : alert(resp.status))
+   .then(resp => resp.status == 200 ? initMap() : alert(resp.status))
    .catch(err => alert(err));
 }
 initMap();
 document.getElementById("titleLeft").onclick = function(){
-   document.querySelectorAll(".addAdventureGUI").forEach(function(e){
+   document.querySelectorAll(".editAdventureGUI").forEach(function(e){
       e.classList.toggle("hide");
    });
 }
--- src/server.go
+++ src/server.go
@@ -8,6 +8,7 @@ import (
    "io/ioutil"
    "crypto/sha256"
    "strconv"
+   "time"
 )
 
 const PORT string = ":80";
@@ -20,6 +21,7 @@ type adventureJson struct {
    Tags string `json:"tags"`
    Lon string `json:"lon"`
    Lat string `json:"lat"`
+   Uid int64 `json:"uid"`
 }
 type geometry struct {
    Coordinates [2]float64 `json:"coordinates"`
@@ -34,14 +36,19 @@ type adventureGeoJson struct {
    Features []feature `json:"features"`
    Type string `json:"type"`
 }
-type newAdventureRequest struct {
+type editAdventureRequest struct {
    Adv adventureJson `json:"adventure"`
    Uname string `json:"uname"`
    Pwd string `json:"pwd"`
 }
+
+var hasWritten = false;
 func sendError(code int, w http.ResponseWriter){
-   w.WriteHeader(code);
-   fmt.Fprint(w, "I am sad to inform you something went wrong");
+   if(!hasWritten){
+      w.WriteHeader(code);
+      fmt.Fprint(w, "I am sad to inform you something went wrong");
+      hasWritten = true;
+   }
    return;
 }
 
@@ -63,37 +70,74 @@ func checkPwd(uname string, pwd string) bool{
 func check(e error, code int, w http.ResponseWriter){
    if(e != nil) {
       sendError(code, w);
-      panic(e);
    }
+}
+func unpackAdventure(w http.ResponseWriter, r *http.Request) (bool, editAdventureRequest){
+   rawBody, err := ioutil.ReadAll(r.Body);
+   check(err, 500, w);
+   var body editAdventureRequest;
+   err = json.Unmarshal(rawBody, &body);
+   check(err, 400, w);
+   if(!checkPwd(body.Uname, body.Pwd)){
+      sendError(403, w);
+      return false, body
+   }
+   return true, body;
+}
+func readAdventureGeoJson(w http.ResponseWriter) adventureGeoJson{
+   b, err := ioutil.ReadFile(ADVENTURE_FILE)
+   check(err, 500, w);
+   var advGeoJson adventureGeoJson;
+   err = json.Unmarshal(b, &advGeoJson);
+   check(err, 500, w);
+   return advGeoJson;
+}
+func writeAdventureGeoJson(w http.ResponseWriter, geojson adventureGeoJson){
+   data, err := json.Marshal(geojson);
+   check(err, 500, w);
+   err = ioutil.WriteFile(ADVENTURE_FILE, data, 0644);
+   check(err, 500, w);
+}
+func adventurePOST(w http.ResponseWriter, r *http.Request){
+   perm, body := unpackAdventure(w, r);
+   if(!perm) {
+      return;
+   }
+   advGeoJson := readAdventureGeoJson(w);
+   newFeature := featureFromAdvReq(body.Adv, w);
+   advGeoJson.Features = append(advGeoJson.Features, newFeature);
+   writeAdventureGeoJson(w, advGeoJson);
+}
+func adventureDELETE(w http.ResponseWriter, r *http.Request){
+   perm, body := unpackAdventure(w,r);
+   if(!perm){
+      return;
+   }
+   advGeoJson := readAdventureGeoJson(w);
+   features := advGeoJson.Features
+   var newFeatures []feature;
+   for _, f := range features {
+      if(f.Properties.Uid != body.Adv.Uid){
+         newFeatures = append(newFeatures, f);
+      }
+   }
+   advGeoJson.Features = newFeatures;
+   writeAdventureGeoJson(w, advGeoJson);
 }
 func adventure(w http.ResponseWriter, r *http.Request){
    switch r.Method {
       case "POST":
-         rawBody, err := ioutil.ReadAll(r.Body);
-         check(err, 500, w);
-         var body newAdventureRequest;
-         err = json.Unmarshal(rawBody, &body);
-         check(err, 500, w);
-         if(!checkPwd(body.Uname, body.Pwd)){
-            sendError(403, w);
-            return;
-         }
-         b, err := ioutil.ReadFile(ADVENTURE_FILE)
-         check(err, 500, w);
-         var advGeoJson adventureGeoJson;
-         err = json.Unmarshal(b, &advGeoJson);
-         newFeature := featureFromAdvReq(body.Adv, w);
-         advGeoJson.Features = append(advGeoJson.Features, newFeature);
-         data, err := json.Marshal(advGeoJson);
-         check(err, 500, w);
-         err = ioutil.WriteFile(ADVENTURE_FILE, data, 0644);
-         check(err, 500, w)
+         adventurePOST(w,r);
+         break;
+      case "DELETE":
+         adventureDELETE(w,r);
          break;
       default:
          sendError(404, w);
    }
 }
 func featureFromAdvReq(req adventureJson, w http.ResponseWriter) feature{
+   req.Uid = time.Now().UnixNano()/1000;
    var geo  geometry;
    geo.Type = "Point";
    lon, err := strconv.ParseFloat(req.Lon,64);
@@ -101,18 +145,16 @@ func featureFromAdvReq(req adventureJson, w http.ResponseWriter) feature{
    lat, err := strconv.ParseFloat(req.Lat,64);
    check(err, 400, w);
    geo.Coordinates = [2]float64{lon, lat};
-   var prop adventureJson;
-   prop = req
    var feat feature;
    feat.Type = "Feature"
    feat.Geometry = geo;
-   feat.Properties = prop;
+   feat.Properties = req;
    return feat;
 }
 func main() {
    fs := http.FileServer(http.Dir("public"))
-   http.Handle("/", fs)
    http.HandleFunc("/adventure", adventure);
+   http.Handle("/", fs)
    fmt.Println("Listening...");
    err := http.ListenAndServe(PORT, nil)
    if(err != nil){
